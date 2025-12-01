143bf53d4dd790a4c9676a12f9a4bbdec7058c0c
GiaHan2005 <ngocgiahannn2005@gmail.com>
2025-12-01 16:27:07 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,59 +1,35 @@
 <?php
 session_start();
+header('Content-Type: application/json');
 include("ketnoidb.php");
+$db = new KETNOI();
 
-$invalid=false;
-if ($_SERVER['REQUEST_METHOD']== 'POST'){
-    $db = new KETNOI();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = trim($_POST['username'] ?? '');
+    $password = $_POST['password'] ?? '';
 
-    $Username = trim($_POST['username'] ?? '');
-    $Matkhau = $_POST['password'] ?? '';
-
-    $nv = $db->check_login($Username, $Matkhau);
-
-    if ($nv !== false && is_array($nv)) {
+    $nv = $db->check_login($username, $password);
+    if($nv !== false && is_array($nv)){
         $_SESSION['HoVaten'] = $nv['HoVaten'];
         $_SESSION['Username'] = $nv['Username'];
         $_SESSION['idNV'] = $nv['idNV'];
-
-        header('Location: admin.php');
-        exit();
-    } else {
-        $invalid = true;
+        echo json_encode(['success'=>true]);
+        exit;
+    }else{
+        echo json_encode(['success'=>false,'message'=>'Đăng nhập sai']);
+        exit;
     }
 }
-?>
-<!DOCTYPE html>
-<html lang="vi">
-<head>
-    <meta charset="UTF-8">
-    <title>Đăng Nhập Nhân Viên</title>
-    <link rel="stylesheet" href="login.css">
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
-</head>
-<body>
 
-    <div id="wrapper">
-        <form action="login.php" id="form-login" method ="post">
-            <h1 class="form-heading">Đăng Nhập</h1>
-            <div class="form-group">
-                <i class="far fa-user"></i>
-                <input type="text" class="form-input"placeholder="Tên đăng nhập" name="username" required>
-            </div>
-                <div class="form-group">
-                <i class="fas fa-key"></i>
-                <input type="password" class="form-input"placeholder="Mật khẩu" name="password" required>
-                 <div id="mat">
-                     <i class="far fa-eye"></i>
-                 </div>
-            </div>
-            <?php if (isset($invalid) && $invalid): ?>
-                <div style="color:red">Đăng nhập sai!!!</div>
-            <?php endif; ?>
-            <input type="submit" value="Bắt đầu làm việc" class="form-submit">
-            <a href = "signup.php">Quên mật khẩu?</a>
-        </form>
-    </div>
-
-</body>
-</html>
+// Kiểm tra session khi frontend load admin
+if($_SERVER['REQUEST_METHOD'] === 'GET'){
+    if(isset($_SESSION['Username'])){
+        echo json_encode([
+            'logged_in'=>true,
+            'HoVaten'=>$_SESSION['HoVaten'],
+            'Username'=>$_SESSION['Username']
+        ]);
+    }else{
+        echo json_encode(['logged_in'=>false]);
+    }
+}
